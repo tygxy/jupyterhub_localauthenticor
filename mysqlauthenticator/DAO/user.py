613aa34d17be7d7f74555b544f0975d6a2be7b77
Guo xingyu <574232205@qq.com>
2019-04-04 16:17:22 +0800

    Add files via upload

--- mysqlauthenticator/DAO/user.py
+++ mysqlauthenticator/DAO/user.py
@@ -1,12 +1,19 @@
 # coding:utf-8
 from sqlalchemy import Column, String, Integer 
 from mysqlauthenticator.DAO.base import Base
+from werkzeug.security import check_password_hash,generate_password_hash
 
 
 class User(Base):
 
-	__tablename__ = 'users'
+    __tablename__ = 'users'
 
-	id = Column(Integer, primary_key=True)
-	username = Column(String(64), nullable=False, unique=True)
-	password = Column(String(64), nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), nullable=False, unique=True)
+    password = Column(String(64), nullable=False)
+
+    def set_password(self, passwd):
+        self.password = generate_password_hash(passwd)
+
+    def check_password(self, passwd):
+        return check_password_hash(self.password, passwd)
